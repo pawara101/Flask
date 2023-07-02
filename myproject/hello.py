049ff7d0f2ba16cd28e834059ab89e1e32a0adb1
pawara101 <pawara101dassanayake@gmail.com>
2023-07-02 12:53:13 +0530

    day 02 Tutorial | flask variale urls

--- myproject/hello.py
+++ myproject/hello.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
-def hello_world():
-   return '<h1>hello Pawara</h1>'
+def helloName(name):
+   return '<h1>Hello %s !!!</h1>' % name
 
 
 if __name__ == '__main__':
